834c3c7deadc5e67755f4b7f296a806b839c9671
Вова <duvla@mail.ru>
2022-12-25 16:17:23 +1000

    task for pr

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         String text = "0,1,2,3,4,5,6,7,8,9,10,11,12,13,";
         System.out.println();
         System.out.println("Произвольная строка имеет вид: "+ text);
+        System.out.println();
         int totalSymbols = text.length();
         if (totalSymbols % 2 == 0) {
             System.out.println("Произвольная строка состоит из " + totalSymbols + " символов. Её можно разделить на две части длиной по " + totalSymbols / 2 + " символов");
